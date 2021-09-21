125dc90cad3d0a12b9ea9577c6eb7f0ef83cb1f8
willschmitz <wms47@georgetown.edu>
2021-09-21 12:08:24 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,12 +3,12 @@
 import random
 
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Hello! Welcome 'Player One' to Rock, Paper, Scissors, Shoot!")
 
 #Promt User for input 
 
 #x=input("Choose 'rock' or 'paper' or 'scissors'")
-user_choice = input("Choose 'rock' or 'paper' or 'scissors'")
+user_choice = input("Please choose 'rock' or 'paper' or 'scissors'")
 print("User Chose:")
 print(user_choice)
 
@@ -16,7 +16,37 @@ print(user_choice)
 
 options = ["rock","paper","scissors"]
 
+#Thanks to Gianna Valencia who shared this in our slack
+if user_choice not in options:
+    print("Not a Valid option")
+    exit()
+
 computer_choice = random.choice(options)
 print("Computer Chose:")
 print(computer_choice)
 
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("Its a tie. Want try again?")
+    if computer_choice == "paper":
+        print("Oh no. Paper beats Rock. You lost this time.")
+    if computer_choice == "scissors":
+        print("Congratulations! Rock beats Scissors. You win!")
+
+if user_choice == "paper":
+    if computer_choice == "paper":
+        print("Its a tie. Want to try again?")
+    if computer_choice == "scissors":
+        print("Oh no. Scissors beats Paper. You lost this time.")
+    if computer_choice == "rock":
+        print("Congratulations! Paper beats Rock. You win!")
+
+if user_choice == "scissors":
+    if computer_choice == "scissors":
+        print("Its a tie. Want to try again?.")
+    if computer_choice == "Rock":
+        print("Oh no. Rock beats Scissors. You lost this time.")
+    if computer_choice == "Paper":
+        print("Congratulations! Scissors beats Paper. You win!")
+
+print("Thanks for playing today. Come back soon to play again.")
